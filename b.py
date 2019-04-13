1953b98fd0843173f52cd93366c2719f1a30a3af
Sunnyjain2403 <47825393+Sunnyjain2403@users.noreply.github.com>
2019-04-13 16:01:03 -0400

    Add files via upload

---
+++ b.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+x=input()
+page = requests.get('https://www.rottentomatoes.com/m/'+x).text
+soup=BeautifulSoup(page,"html.parser")
+article=soup.find('span',class_='mop-ratings-wrap__percentage').text
+article2=soup.find('span',class_='mop-ratings-wrap__percentage mop-ratings-wrap__percentage--audience').text
+print("critic rating:"+article)
+print("audience  rating"+article2)
+
+
