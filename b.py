1443b3f7996c0b46a30014f1383521ba89d5fee4
Sunnyjain2403 <47825393+Sunnyjain2403@users.noreply.github.com>
2019-04-14 07:41:40 -0400

    Update b.py

--- b.py
+++ b.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-x=input("enter the movie(*no letter in capital):")
-page = requests.get('https://www.rottentomatoes.com/m/'+x).text
+x=input("enter the movie name:")
+
+page = requests.get('https://www.rottentomatoes.com/m/'+x.replace(' ','_').lower()).text
 soup=BeautifulSoup(page,"html.parser")
 article=soup.find('span',class_='mop-ratings-wrap__percentage').text
 article2=soup.find('span',class_='mop-ratings-wrap__percentage mop-ratings-wrap__percentage--audience').text
-print("critic rating:"+article)
-print("audience  rating"+article2)
-
-
+print("critic rating:"+article.strip())
+print("audience rating:"+article2.strip())
